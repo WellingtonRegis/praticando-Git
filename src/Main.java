a330e98c7ed20a5f474b84881c1c26e97dab322e
WellingtonRegis <ricardowellington854@gmail.com>
2022-06-03 02:23:53 -0300

    criando uma classe pessoa para armazenar dados

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Digite seu nome :");
+        String nome = sc.next();
+        System.out.println("Digite sua idade :");
+        int idade = sc.nextInt();
+        System.out.println("Digite seu email :");
+        String email = sc.next();
+
+        System.out.println("Você digitou o nome: " + nome + " Sua idade é: " + idade + " Seu email é: " + email);
     }
 }
